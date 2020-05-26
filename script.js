1fae09ede22a56c5cbee3eb195fe8803da4a714e
mtorres9095 <mtorres9095@gmail.com>
2020-05-26 11:38:31 -0400

    Defined variables from form and if statement with NaNs

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 // Write your JavaScript code here!
+window.addEventListener("load", function() {
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function(event) {
+      let pilotNameInput = document.querySelector("input[name=pilotName]");
+      let copilotNameInput = document.querySelector("input[name=copilotName]");
+      let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+      let cargoMassInput = document.querySelector("input[name=cargoMass]");
+      if (pilotNameInput.value === "" || copilotNameInput.value === "" ||isNaN(fuelLevelInput) || isNaN(cargoMassInput) ) {
+         alert("All fields are required!");
+         event.preventDefault();
+
+      }
+   });
+});
+
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
